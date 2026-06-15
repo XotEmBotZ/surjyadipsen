752b5f45a859a64d6789149f9b8a18bc933c354f
XotEmBotZ <xotembotzop@gmail.com>
2026-06-15 09:37:37 +0530

    feat: updated robots.txt to disallow few urls

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
+      disallow: ["/api/", "/keystatic/", "/_next/", "/ingest/"],
     },
     sitemap: `${baseUrl}/sitemap.xml`,
   };
